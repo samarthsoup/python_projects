3edf14c6a3800e3eb2bc5874a89b76be0f8fa8d0
samarth <samarthkulkarni@icloud.com>
2024-01-20 19:25:04 +0530

    one more step ahead

--- formula1_web_scraper.py
+++ formula1_web_scraper.py
@@ -14,7 +14,11 @@ def get_constructors_championship(year):
     if table:
         for row in table.find_all('tr')[1:]:
             data = [cell.get_text(strip=True) for cell in row.find_all('td')]
-            formatted_data.append({'position': data[1],'team': data[2], 'points': int(data[3])})
+            formatted_data.append({
+                'position': data[1],
+                'team': data[2], 
+                'points': int(data[3])
+                })
         return formatted_data
     else:
         return f"no data available for the year {year}"
@@ -33,7 +37,11 @@ def get_drivers_championship(year):
             full_name = ' '.join(part.get_text(strip=True) for part in name_parts if part.get_text(strip=True))
             points = row.find_all('td')[5].get_text(strip=True)
             position = row.find_all('td')[1].get_text(strip=True)
-            formatted_data.append({'position': position,'driver': full_name, 'points': int(points)})
+            formatted_data.append({
+                'position': position,
+                'driver': full_name, 
+                'points': int(points)
+                })
         return formatted_data
     else:
         return f"no data available for the year {year}"
@@ -52,10 +60,38 @@ def get_drivers_championship_code(year):
             full_name = ' '.join(part.get_text(strip=True) for part in name_parts if part.get_text(strip=True))
             points = row.find_all('td')[5].get_text(strip=True)
             position = row.find_all('td')[1].get_text(strip=True)
-            formatted_data.append({'position': position,'driver': full_name, 'points': int(points)})
+            formatted_data.append({
+                'position': position,
+                'driver': full_name, 
+                'points': int(points)
+                })
         return formatted_data
     else:
         return f"no data available for the year {year}"
+
+def get_driver_at_position(year, position):
+    url = f'https://www.formula1.com/en/results.html/{year}/drivers.html'
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find('table', class_='resultsarchive-table')
+
+    if table:
+        try:
+            row = table.find_all('tr')[position]  
+            name_parts = row.find_all('span', class_=lambda x: x in ["hide-for-tablet", "hide-for-mobile"])
+            full_name = ' '.join(part.get_text(strip=True) for part in name_parts)
+            points = row.find_all('td')[5].get_text(strip=True)  
+            return {
+                'position': position,
+                'driver': full_name,
+                'points': int(points)
+            }
+        except IndexError:
+            return f"no driver data available for position {position} in the year {year}"
+    else:
+        return f"no data available for the year {year}"
+
 
 while True:
     user_input = input("?")
@@ -85,5 +121,13 @@ while True:
             drivers_list = get_drivers_championship_code(year)
             for row in drivers_list:
                 print(f"{row['position']}.{row['driver']}: {row['points']}")
+        except ValueError:
+            print("enter valid year")
+    elif len(parts) == 4 and parts[0].lower() == 's' and parts[1].lower() == 'd':
+        try:
+            year = int(parts[2])
+            pos = int(parts[3])
+            driver = get_driver_at_position(year, pos)
+            print(f"{driver['driver']}")
         except ValueError:
             print("enter valid year")
